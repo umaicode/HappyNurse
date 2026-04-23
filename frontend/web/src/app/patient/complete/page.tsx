538440ffb1f185e12e74202d866c610ae7eafcda
문현지 <sahacar2000@naver.com>
2026-04-23 15:08:22 +0900

    [FE] feat: 환자웹 퍼블리싱 진행중, node버전 20 -> 22 변경

---
+++ frontend/web/src/app/patient/complete/page.tsx
@@ -0,0 +1,5 @@
+import Complete from '@/components/patient/complete'
+
+export default function CompletePage() {
+  return <Complete />
+}
---
+++ frontend/web/src/app/patient/page.tsx
@@ -0,0 +1,5 @@
+import Auth from '@/components/patient/auth'
+
+export default function PatientPage() {
+  return <Auth />
+}
