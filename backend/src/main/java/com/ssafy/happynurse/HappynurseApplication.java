b0fb8d40acc8e8fa5c1ef059206423d1ed0d90b0
이승연 <lsy7430@naver.com>
2026-04-24 12:22:19 +0900

    Revert "[BE] feat : 만료 리프레시 토큰 정리 스케줄러 추가"
    This reverts commit bb63ef812a6b8a203a55dac5c8f8e8826e88e246.

--- backend/src/main/java/com/ssafy/happynurse/HappynurseApplication.java
+++ backend/src/main/java/com/ssafy/happynurse/HappynurseApplication.java
@@ -2,10 +2,8 @@ package com.ssafy.happynurse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class HappynurseApplication {
 
 	public static void main(String[] args) {
