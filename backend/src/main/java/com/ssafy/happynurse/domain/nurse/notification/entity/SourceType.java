d9c0f95a7626bef16f96f6857a47232ffd584e1d
umaicode <park.seungchan99@gmail.com>
2026-05-15 22:22:07 +0900

    [BE] feat: 간호기록 SSE를 위한 SourceType에 nursing_record 추가

--- backend/src/main/java/com/ssafy/happynurse/domain/nurse/notification/entity/SourceType.java
+++ backend/src/main/java/com/ssafy/happynurse/domain/nurse/notification/entity/SourceType.java
@@ -1,5 +1,5 @@
 package com.ssafy.happynurse.domain.nurse.notification.entity;
 
 public enum SourceType {
-    self_report, iv_alert, timer, order_change, vital_alert, web_login, web_logout
+    self_report, iv_alert, timer, order_change, vital_alert, web_login, web_logout, nursing_record
 }
